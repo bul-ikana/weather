45bdd749a8e2e38322c6b3527e9222d326dca35b
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-27 20:36:01 -0500

    Move weather card template to component

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
+//                      //
+// Component definition //
+//                      //
+
+// Weather card //
+
+const weather = Vue.component('weather', {
+	template: '#weather'
+})
+
+
 //                //
 // Vue definition //
 //                //
