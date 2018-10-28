623b9b8501d36f64ed1f7b3fcf8e4e8244cdb000
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-27 21:24:45 -0500

    Give woeid to components as props and let them fetch their own data

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+//                         //
+// Basic app configuration //
+//                         //
+const API_URL = "http://localhost:8080/weather/weather.php";
+
 //                      //
 // Component definition //
 //                      //
@@ -5,15 +10,46 @@
 // Weather card //
 
 const weather = Vue.component('weather', {
-	template: '#weather',
+  template: '#weather',
 
-	props: [
-		'title',
-		'the_temp',
-		'min_temp',
-		'max_temp',
-		'weather_state_abbr'
-	]
+  data () {
+    return {
+      title: '',
+      the_temp: '',
+      min_temp: '',
+      max_temp: '',
+      weather_state_abbr: ''
+    }
+  },
+
+  props: [
+    'woeid'
+  ],
+
+  mounted () {
+    axios
+      .get(API_URL, {
+        params: {
+          command: 'location',
+          woeid: this.woeid
+        }
+      })
+      .then( response => {
+        this.title = response.data.title
+        this.the_temp = response.data.consolidated_weather[0].the_temp
+        this.min_temp = response.data.consolidated_weather[0].min_temp
+        this.max_temp = response.data.consolidated_weather[0].max_temp
+        this.weather_state_abbr = response.data.consolidated_weather[0].weather_state_abbr
+      })
+  },
+
+  filters: {
+    celcius: function (value) {
+      if (!value) return ''
+
+      return Math.round(value) + '°'
+    }
+  }
 })
 
 
@@ -23,4 +59,4 @@ const weather = Vue.component('weather', {
 
 var vm = new Vue({
   el: '#app'
-});
+})
