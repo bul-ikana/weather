1b675223a0f930b679e639dda3548d7e6ece04b9
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-28 13:34:38 -0600

    Show weather data on details page

--- code.js
+++ code.js
@@ -45,7 +45,31 @@ const weather = Vue.component('weather', {
   },
 
   filters: {
-    celcius: function (value) {
+    celsius: function (value) {
+      if (!value) return ''
+
+      return Math.round(value) + '°'
+    }
+  }
+})
+
+const weatherline = Vue.component('weatherline', {
+  template: '#weatherline',
+
+  props: [
+    'applicable_date',
+    'weather_state_abbr',
+    'the_temp',
+    'min_temp',
+    'max_temp'
+  ],
+
+  filters: {
+    dayofweek: function (value) {
+      if (!value) return ''
+      return moment(value).format('dddd')
+    },
+    celsius: function (value) {
       if (!value) return ''
 
       return Math.round(value) + '°'
@@ -64,7 +88,34 @@ const detailspage = Vue.component('detailspage', {
 
   props: [
     'woeid'
-  ]
+  ],
+
+  data () {
+    return {
+      title: '',
+      consolidated_weather: []
+    }
+  },
+
+  mounted () {
+    axios
+      .get(API_URL, {
+        params: {
+          command: 'location',
+          woeid: this.woeid
+        }
+      })
+      .then( response => {
+        console.log(response.data)
+        this.title = response.data.title
+        this.consolidated_weather = response.data.consolidated_weather
+        // this.the_temp = response.data.consolidated_weather[0].the_temp
+        // this.min_temp = response.data.consolidated_weather[0].min_temp
+        // this.max_temp = response.data.consolidated_weather[0].max_temp
+        // this.weather_state_abbr = response.data.consolidated_weather[0].weather_state_abbr
+        // this.loading = false
+      })
+  }
 })
 
 //                   //
